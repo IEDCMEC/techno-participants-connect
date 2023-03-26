9f180d31d9228fbce3239ff07aae07e6940f0346
Allen Shibu <93600615+alapanoski@users.noreply.github.com>
2023-03-26 09:59:09 +0530

    Final

--- src/pages/api/users/[id].js
+++ src/pages/api/users/[id].js
@@ -1,27 +1,49 @@
-import axios from "axios";
+import axios from "axios"
+const fs = require("fs")
+import SupabaseClient from "@/utils/SupabaseClient"
 
-import SupabaseClient from "@/utils/SupabaseClient";
+const rp = require("request-promise")
+const csvtojson = require("csvtojson")
+
+// export default async function pConnect(req, res) {
+//   const csv = await rp(process.env.SHEET_URL)
+//   const json = await csvtojson().fromString(csv)
+
+//   res.status(200).json({
+//     paricipantDetails: json,
+//   })
+// }
 
 export default async function handler(req, res) {
+  const csv = await rp(process.env.SHEET_URL)
+  const json = await csvtojson().fromString(csv)
+
+  // fs.writeFile("jsonData", JSON.stringify(json), function (err) {
+  //   if (err) {
+  //     return console.log(err)
+  //   }
+  //   console.log("The file was saved!")
+  // })
+  // console.log(json)
+  //const json = require("./jsonData")
+  console.log(json)
+  const { id } = req.query
+  const { data } = await SupabaseClient.from("register")
+    .select("*, users(*) ")
+    .eq("band_id", id)
+
   if (req.method === "GET") {
-    const { id } = req.query;
-    const users = await SupabaseClient.from("register")
-      .select("*, users(email) ")
-      .eq("band_id", id);
+    console.log(data[0])
+    const selectedUser = data[0]?.users
+    console.log({ selectedUser })
 
-    const response = await axios.get("/api/participantconnect");
+    const selectedParticipantData = json.find(
+      (participant) => participant.Email === selectedUser?.email
+    )
 
-    const participantsData = response.data;
-
-    participantsData.forEach((participant) => {
-      const user = users.find((user) => user.id === participant.id);
-      if (user) {
-        console.log("Do something");
-      }
-    });
-
-    res.status(200).json(users);
+    console.log({ selectedParticipantData })
+    res.status(200).json({ user: selectedParticipantData })
   } else {
-    res.status(405).json({ message: "Method not allowed" });
+    res.status(405).json({ message: "Method not allowed" })
   }
 }
