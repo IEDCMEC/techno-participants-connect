ada45123b6013d4d4dfc84acd1d2785b12e263b2
Aldrin Jenson <aldrinjenson@gmail.com>
2023-03-26 10:53:21 +0530

    fix issue🤞

--- src/pages/api/users/[id].js
+++ src/pages/api/users/[id].js
@@ -1,9 +1,9 @@
-import axios from "axios"
-const fs = require("fs")
-import SupabaseClient from "@/utils/SupabaseClient"
+import axios from "axios";
+const fs = require("fs");
+import SupabaseClient from "@/utils/SupabaseClient";
 
-const rp = require("request-promise")
-const csvtojson = require("csvtojson")
+const rp = require("request-promise");
+const csvtojson = require("csvtojson");
 
 // export default async function pConnect(req, res) {
 //   const csv = await rp(process.env.SHEET_URL)
@@ -15,8 +15,8 @@ const csvtojson = require("csvtojson")
 // }
 
 export default async function handler(req, res) {
-  const csv = await rp(process.env.SHEET_URL)
-  const json = await csvtojson().fromString(csv)
+  const csv = await rp(process.env.SHEET_URL);
+  const json = await csvtojson().fromString(csv);
 
   // fs.writeFile("jsonData", JSON.stringify(json), function (err) {
   //   if (err) {
@@ -26,24 +26,28 @@ export default async function handler(req, res) {
   // })
   // console.log(json)
   //const json = require("./jsonData")
-  console.log(json)
-  const { id } = req.query
+  // console.log(json)
+
+  const { id } = req.query;
   const { data } = await SupabaseClient.from("register")
     .select("*, users(*) ")
-    .eq("band_id", id)
+    .eq("band_id", id);
+
+  // console.log(data);
 
   if (req.method === "GET") {
-    console.log(data[0])
-    const selectedUser = data[0]?.users
-    console.log({ selectedUser })
+    try {
+      const selectedUser = data[0]?.users;
+      const selectedParticipantData = json.find((participant) => {
+        return participant.Email === selectedUser?.email;
+      });
 
-    const selectedParticipantData = json.find(
-      (participant) => participant.Email === selectedUser?.email
-    )
-
-    console.log({ selectedParticipantData })
-    res.status(200).json({ user: selectedParticipantData })
+      res.status(200).json({ user: selectedParticipantData });
+    } catch (error) {
+      console.log(error);
+      res.status(400).send(error);
+    }
   } else {
-    res.status(405).json({ message: "Method not allowed" })
+    res.status(405).json({ message: "Method not allowed" });
   }
 }
